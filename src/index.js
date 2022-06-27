692500339a445ddd2ab0f38d168dbf36d3931ef8
HeleneElena <heleneebert1@gmail.com>
2022-06-27 13:27:21 +0200

    commit9

--- src/index.js
+++ src/index.js
@@ -124,11 +124,24 @@ function actionPage() {
 
 // получение данных с сервера
 function getData() {
-    fetch('../db/db.json');
+    const goodsWrapper = document.querySelector('.goods');
+   fetch('../db/db.json').then((response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error ('Es gibt einige Fehler: ' + response.status);
+        }
+    })
+    .then(data => console.log(data))
+    .catch(err =>{
+         console.warn(err); // об ошибке сообщение разработчику
+         goodsWrapper.innerHTML = '<div style="color:red; font-size:2rem" >Etwas ist schief gelaufen</div>'; // об ошибке сообщение user
+    });
 }
 // end получение данных с сервера
 
 getData();
+
 toggleCheckbox();
 toggleCard();
 addCart();
